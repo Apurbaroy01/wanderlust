7b0972803a35cfbe0aa247fc453212a5d734d37f
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-05-16 23:46:32 +0600

    fix

--- src/components/DestinationCard/BookingCard.jsx
+++ src/components/DestinationCard/BookingCard.jsx
@@ -1,14 +1,48 @@
 "use client";
-import {Calendar} from "@gravity-ui/icons";
-import {DateField, Label} from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+import { Calendar } from "@gravity-ui/icons";
+import { DateField, Label } from "@heroui/react";
+import { useState } from "react";
 
 
 const BookingCard = ({ destination }) => {
+
+    const { data: session } = authClient.useSession();
+    const [date, setDate] = useState(null);
+
+
+    const handleBooking = async () => {
+        if (!session?.user) {
+            alert("Please log in to book this destination.");
+            return;
+        }
+
+        const BookingData = {
+            userId: session.user.id,
+            destinationId: destination._id,
+            date: new Date(date),
+        };
+        // Handle booking logic here
+        const res = await fetch("http://localhost:5000/bookings", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(BookingData),
+        });
+        if (res.ok) {
+            alert("Booking successful!");
+        } else {
+            alert("Failed to book. Please try again.");
+        }
+
+        console.log("Booking Data:", res);
+    };
     return (
         <div className="space-y-4">
             <h2 className="text-2xl font-bold text-center">Booking Card</h2>
-            <p>{destination.price} per person</p>
-            <DateField className="w-[256px]" name="date">
+            <p className="font-semibold">${destination.price} per person</p>
+            <DateField onChange={setDate} className="w-[256px]" name="date">
                 <Label>Date<spnan className="text-red-500">*</spnan></Label>
                 <DateField.Group>
                     <DateField.Prefix>
@@ -18,7 +52,9 @@ const BookingCard = ({ destination }) => {
                 </DateField.Group>
             </DateField>
             <div className="flex w-full gap-2">
-                <button className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                <button type="button"
+                    onClick={handleBooking}
+                    className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
                     Book Now
                 </button>
             </div>
